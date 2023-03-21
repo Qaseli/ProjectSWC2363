e0c2d59d13efb392ec9aa7a8486d96884099b207
Qaseli <107803031+Qaseli@users.noreply.github.com>
2023-03-22 00:28:55 +0800

    Add files via upload

---
+++ SWCAKMAL/choose.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Chat System</title>
+<body style="background-image: url('1.jpg');">
+    <font size="5pt" face="Calibri" color="black">
+</head><center>
+    <p style="font-family:'Times New Roman'" >Hello you are Higly Welcome, this is a simple chatting system created in other to make information transmission very easily please don't use this as an opportunity to abuse,verbal malteatment, assault, displaying attempts to harbour irrelevant materials/links, violation or  crime if found with one of the followings you will definately banned from accessing!!</p>
+<br />
+<p><h1 style="color:rgb(255, 68, 0);">Choose Below to Continue</h1><a><strong>
+<a href="@@cht" style="color:rgb(250, 250, 250);" style="font-family:'Times New Roman'" >Group Chat</p></a>
+<div>
+    <div class="symbol">
+    <tr><a href="https://www.facebook.com/kuptmkl.official"><img src="https://www.kuptm.edu.my/images/KUPTM/social/facebook1png.png" style="width:50px;height:50px;"></a></tr>
+    <tr><a href="https://www.instagram.com/kuptmkl_official/"><img src="https://www.kuptm.edu.my/images/KUPTM/social/insta1png.png" style="width:50px;height:50px;"></a></tr>
+    <tr><a href="https://twitter.com/kptm_official"><img src="https://www.kuptm.edu.my/images/KUPTM/social/twitter1png.png" style="width:50px;height:50px;"></a></tr>
+    </div>
+</div>
+<br /><br>
+</br></br><a href="logout.php" style="color:rgb(255, 255, 255);" style="font-family:'Times New Roman'">Log Out</a>
+<body>
+</body>
+<br /><br><br /><br><br /><br><br />
+<p><Strong><center> Programmed and Design by Zul Azri Bin Jalalludin</center></Strong></p>
+</html>
